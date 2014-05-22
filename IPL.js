1e81b80b4909cf89a68ae981b1309c1093f5883b
rahuljaincse <jenumjaytishashnum>
2014-05-22 14:39:44 +0530

    newly added

---
+++ IPL.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// Write your code below!
+    var ipl = prompt("Type Your Case");
+    switch (ipl){
+    case 'Teams':
+    console.log("There Are 8 Teams IN IPL");
+    break;
+
+    case'Owners':
+    console.log("All Teams Have An Owner");
+    break;
+    
+    case'Players':
+    console.log("Every Team Has 11 Playes 4 are Overseas Players In Every Team");
+    break;
+    
+    default:
+    console.log(" Welcome To Indian Premier League");
+    }
+
+
+
